172d1850f8c2e052872bbb00f523bd80d423402b
LY <1253300251@qq.com>
2023-09-21 17:04:49 +0800

    The simplest traversal

--- 01_1素数个数统计（埃筛法）.py
+++ 01_1素数个数统计（埃筛法）.py
@@ -6,7 +6,7 @@ x = [False for i in range(int(n))]
 for i in range(2, int(n)):
     if not x[i-1]:
         count += 1
-        j = 2 * i
+        j = i * i  # 最简遍历方法
         while j < int(n):  # 将x数组下标为i的整数倍的都不执行count++，i从2到n
             x[j-1] = True
             j += i
